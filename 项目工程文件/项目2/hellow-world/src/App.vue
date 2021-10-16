8d1a5fef135c10b9aac1f87ddc346f8747c5ebc7
Chuang Wang <wangc@alayadata.com>
2021-10-16 21:00:02 +0800

    ccc

---
+++ 项目工程文件/项目2/hellow-world/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Author url="https://cli.vuejs.org/cli-new-project.png" :size="100"/>
+    <Author url="https://cli.vuejs.org/cli-new-project.png" :size="200"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Author from './components/Author.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Author
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 项目工程文件/项目2/hellow-world/src/components/Author.vue
@@ -0,0 +1,29 @@
+<template>
+  <img class="image" :src="url" alt="这是头像" :style="{
+      width:size + 'px',
+      height:size + 'px'
+  }">
+</template>
+
+<script>
+export default {
+    props:{
+        url:{
+            type:String,
+            required:true
+        },
+        size:{
+            type:Number,
+            required:true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.image{
+    border-radius: 50%;
+    object-fit: cover;
+    display: block;
+}
+</style>
